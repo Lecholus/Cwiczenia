d09b5f703421a739e78ec50f4774e3d0979b2d23
Nameless <adamiewa.3010@gmail.com>
2023-12-20 21:42:55 +0100

    ch.1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,42 +1,49 @@
+// App.jsx
+
+import React from 'react';
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} - Exercises: {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return (
+    <p>Total exercises: {totalExercises}</p>
+  );
+};
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // Assuming you have a variable `course` defined with the necessary data
 
   return (
     <div>
-      <Header cour = {course} />
-      <Content ptsd = {course} />
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
-  )
-}
+  );
+};
 
-const Header = ({cour}) => {
-  return(
-    <p>{cour.name}</p>
-
-  )
-}
-
-const Content = ({ptsd}) => {
-  return(
-    <p>{ptsd.parts.name}</p>
-    
-  )
-}
-
-export default App
+export default App;
